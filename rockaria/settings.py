fa01a09d29144d5a3d2dd8cb5bee69d8ea3a9c51
curran68 <mac1968@gmail.com>
2025-06-05 20:09:03 +0100

    added home app and templates

--- rockaria/settings.py
+++ rockaria/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,10 @@ ROOT_URLCONF = 'rockaria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Add this line
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
