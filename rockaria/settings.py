248c1550dff5ecb9c60ea8a0b6b266f03d215187
Mark <164265787+curran68@users.noreply.github.com>
2025-07-05 14:32:21 +0000

    changes to login pages

--- rockaria/settings.py
+++ rockaria/settings.py
@@ -91,10 +91,7 @@ ROOT_URLCONF = 'rockaria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-           # os.path.join(BASE_DIR, 'templates', 'allauth'),
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,7 +99,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.static',  # make sure this is present
+                'django.template.context_processors.static',  # Ensure this is present
                 'home.context_processors.currency',
             ],
         },
