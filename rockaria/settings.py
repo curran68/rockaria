a0976bdd37d850b33a3b60ace473f88b8eec98bc
Mark <164265787+curran68@users.noreply.github.com>
2025-07-07 10:16:27 +0000

    saving changes

--- rockaria/settings.py
+++ rockaria/settings.py
@@ -116,7 +116,13 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yourprovider.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 
 ACCOUNT_LOGIN_METHODS = {'email', 'username'}
 
